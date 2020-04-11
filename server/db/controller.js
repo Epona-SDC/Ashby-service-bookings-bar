b7ec7ce33a14887505575ff1cbe55f1e989150c8
Ashby Gunter <theshbee@yahoo.com>
2020-04-11 12:34:48 -0700

    Add pseudocode for makeNewRental

--- server/db/controller.js
+++ server/db/controller.js
@@ -75,16 +75,34 @@ const getOneRentalAndDates = (id) => {
     });
 }
 
+const makeNewRental = (rentalInfo) => {
+  // ignore if an id is passed in, client doesn't get to decide that
+  // some data validation
+  // price: exists, valid real
+  // max_guests: exists, valid int
+  // cleaning_fee: exists, valid real
+  // service_fee: exists, valid real
+  // occupancy_fee: exists, valid real
+  // if no numReviews, default to 0 and set avgStars same
+    // else: both valid int / real
+  // use Rental to update basic info
+
+  // put dates in order
+  // loop through and check that dates are valid
+    // add each valid date
+}
+
+
 const addDateToRental = (rentalId, date) => {
   // get id for date from UpcomingDate table
   // insert a line into RentalDate using renatalId and dateId
 }
 
-const makeNewRental = (rentalInfo) => {
-  // add entry to Rental using basic info
-  // loop through array of dates
-    // call addDateToRental for each
-  // use promiseAll?
+const updateRental = (rentalInfo) => {
+  // verify id & fetch record
+  // validate updates
+  // make changes
+  // save updates
 }
 
 const deleteRental = (id) => {
@@ -92,13 +110,6 @@ const deleteRental = (id) => {
   // tables set to cascade deletion automagically, yay!
 }
 
-const updateRental = () => {
-  // use Rental to update basic info
-
-  // fetch dates for the rental
-  // compare the lists
-  // add / delete as necessary
-}
 
 
 exports.getOneJustRental = getOneJustRental;
